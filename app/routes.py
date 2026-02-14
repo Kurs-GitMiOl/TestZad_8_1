9eaea6c908a2907b63ba7b72d66d2af6de3390eb
Kurs GitMiOl <mirek_olszowka_git@o2.pl>
2026-02-14 14:35:22 +0100

    commit_ready_system1.py

--- app/routes.py
+++ app/routes.py
@@ -19,10 +19,10 @@ router = APIRouter()
 
 # Input data schema
 class IrisInput(BaseModel):
-    sepal_length: float = Field(..., ge=4.3, le=7.9)
-    sepal_width: float = Field(..., ge=2.0, le=4.4)
-    petal_length: float = Field(..., ge=1.0, le=6.9)
-    petal_width: float = Field(..., ge=0.1, le=2.5)
+    sepal_length: float = Field(..., ge=4.3, le=7.9, description="Sepal length must be between 4.3 and 7.9 cm")
+    sepal_width: float = Field(..., ge=2.0, le=4.4, description="Sepal width must be between 2.0 and 4.4 cm")
+    petal_length: float = Field(..., ge=1.0, le=6.9, description="Petal length must be between 1.0 and 6.9 cm")
+    petal_width: float = Field(..., ge=0.1, le=2.5, description="Petal with must be between 0.1 and 2.5 cm")
 
 
 
